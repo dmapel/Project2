bffb76681f34a53cd5b80bc3a599861c5249e65f
jonwuzulike <jasonwuzulike@gmail.com>
2018-09-06 18:54:51 -0400

    Working on Cognito within the app.

--- project2/src/app/login/login.component.ts
+++ project2/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+//import { CognitoService } from '../service/cognito.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,22 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  img = "assets/pics/revature-logo-600x219.png"
+  img = "assets/pics/revature-logo-600x219.png";
+  email : string;
+  password: string;
 
   constructor() { }
 
   ngOnInit() {
-  }
+   // window.location.href= "https://revaturetech.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=4ptb0da4skq58fmigvjp65o1k&redirect_uri=http://localhost:4200/profile";
+ }
+//Attempt to sign in a user.
+login() {
+console.log(this.email);
+console.log(this.password);
+}
+
+
 
 
 }
