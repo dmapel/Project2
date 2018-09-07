7970cac6dd8115326213bc49a8d55e649ed17c57
Tosino95 <40637026+Tosino95@users.noreply.github.com>
2018-09-07 14:56:17 -0500

    Tosin (#5)
    * Tosin
    
    * Testing branch
    
    * Revert "Merge pull request #1 from dmapel/beck"
    
    This reverts commit c6414ae52a5040615a41f01d6afb7f612b61efa2, reversing
    changes made to 8342feb229dcb35f9541d84cec554daafc6a00ca.
    
    * bare bones client side
    
    * Setting up Angular Material
    
    * clientSide update
    
    * Delete.
    
    * Addes create page and template page
    
    * Merging
    
    * Working on Cognito within the app.
    
    * Clean up for merge
    
    * mocked data
    
    * deleted SettingUpAngularMaterial

---
+++ project2/src/app/service/app-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppServiceService {
+
+  constructor() { }
+
+// Method that allows a user to create a new page.
+createNewPage() {
+}
+
+
+
+
+}
