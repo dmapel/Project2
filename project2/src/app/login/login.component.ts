b7a8cdf168f228408bfdc519849bd81a50e5a315
jonwuzulike <jasonwuzulike@gmail.com>
2018-09-14 20:53:35 -0400

    Functionalty to create a page and see it on the native page.

--- project2/src/app/login/login.component.ts
+++ project2/src/app/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
     // window.location.href= "https://revaturetech.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=4ptb0da4skq58fmigvjp65o1k&redirect_uri=http://localhost:4200/profile";
   }
  //Attempt to sign in a user.
+  //Attempt to sign in a user.
  login() {
   console.log(this.username);
   console.log(this.password);
@@ -34,34 +35,37 @@ export class LoginComponent implements OnInit {
   this.cognitoService.signIn(this.username, this.password).subscribe(
     result => {
       if (result) {
+        this.userService.getUserByUsername(this.username, this.password).subscribe(
+          user => {
+            console.log(user);
+            if (user) {
+              if (this.username === user.username)
+              console.log('Found in database.');
+              
+              //Set the current user.
+              this.userService.setCurrentUser(user);
+              //Navigates to the search page or admin page depending on postion id.
+              if (user.posId == 1) {
+              this.router.navigate(['admin-profile']);
+              }else {
+              this.router.navigate(['search-bar']);
+              }
+            }
+            else {
+              console.log("User not found in database.");
+            }
+          }
+        )
         // If there was an error
         if (result['message']) {
           this.errorMessage = 'Invalid credentials';
-          this.display="Username or password not valid. Please try again.";
+          alert("Username or password not valid. Please try again.");
           return;
         }
       }
     })
 
-    this.userService.getUserByUsername(this.username, this.password).subscribe(
-      user => {
-        console.log(user);
-        if (user) {
-          if (this.username === user.username)
-          console.log('Hits');
-          sessionStorage.setItem('user', JSON.stringify(user));
-          this.userService.user.next(user);
-          if (user.posId==2){
-            this.router.navigate(['search-bar'])
-          } else {
-            this.router.navigate(['admin-profile'])
-          }
-        }
-        else {
-          console.log("User not found in database.");
-        }
-      }
-    )
+   
 
 
 
