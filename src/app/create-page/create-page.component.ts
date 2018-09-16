f93660fc706303a446f0c96327c08f71284eef89
dmapel <40637038+dmapel@users.noreply.github.com>
2018-09-16 18:02:34 -0400

    Update create-page.component.ts

--- src/app/create-page/create-page.component.ts
+++ src/app/create-page/create-page.component.ts
@@ -1,4 +1,3 @@
-import { NewPage } from '../models/new-page';
 import { Page } from '../models/page';
 import { NewPageService } from './../service/new-page.service';
 import { HttpClient } from '@angular/common/http';
