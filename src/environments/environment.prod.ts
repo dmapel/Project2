5ebfc7b56c50c5c413d06a991db57f1e94da5321
dmapel <40637038+dmapel@users.noreply.github.com>
2018-09-16 18:20:32 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  envName: `dev`,
+  apiUrl: 'http://ec2-18-188-229-73.us-east-2.compute.amazonaws.com:8080/',
+  context: `http://localhost:4200/swagger-ui.html`
 };
