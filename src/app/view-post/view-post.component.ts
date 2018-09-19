3a7842e7f2c3dcb8c3b3998c157f98c0b5cd4b23
jonwuzulike <jasonwuzulike@gmail.com>
2018-09-19 00:19:50 -0400

    View pages of selected tag functionality;

--- src/app/view-post/view-post.component.ts
+++ src/app/view-post/view-post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PageService } from '../service/page.service';
 import { Page } from '../models/page';
+import { NewPageService } from '../service/new-page.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-post',
@@ -13,7 +15,7 @@ export class ViewPostComponent implements OnInit {
   title2 : string;
 
 
-  constructor(private pageService: PageService) { }
+  constructor(private pageService: PageService, private nPageService : NewPageService, private router: Router) { }
 
   ngOnInit() {
 
@@ -26,6 +28,18 @@ export class ViewPostComponent implements OnInit {
      //Getting the pages titles.
      this.title2 = this.pages[0][0].title;
     this.title= this.pages[1][0].title;
+  }
+
+  first () {
+    this.nPageService.setPage(this.pages[1][0])
+    console.log(this.pages[1][0]);
+    this.router.navigate(['page']);
+  }
+
+  second () {
+    this.nPageService.setPage(this.pages[0][0])
+    console.log(this.pages[0][0]);
+    this.router.navigate(['page']);
   }
 
 }
